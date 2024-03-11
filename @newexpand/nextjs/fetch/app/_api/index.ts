8a9dc1b117acefaec20af8e280723f62c251da65
newExpand <magin0auto@gmail.com>
2024-03-11 18:02:12 +0900

    custom fetch 함수 추가

---
+++ @newexpand/nextjs/fetch/app/_api/index.ts
@@ -0,0 +1,1 @@
+import extendFetch from "newexpand-nextjs-fetch";
--- @newexpand/nextjs/fetch/app/page.tsx
+++ @newexpand/nextjs/fetch/app/page.tsx
@@ -1,3 +1,6 @@
 export default function Page() {
+  // const data = extendFetch.get("https://jsonplaceholder.typicode.com/posts/1");
+
+  // console.log(data);
   return <h1>Hello, Next.js!</h1>;
 }
