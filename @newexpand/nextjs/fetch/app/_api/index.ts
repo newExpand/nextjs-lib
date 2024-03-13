46e72bf3407f6a0ef15ce88ca25dfb62758712f3
newExpand <magin0auto@gmail.com>
2024-03-13 11:48:06 +0900

    버전 업그레이드 기능 추가 및 타입 에러 사항 변경

--- @newexpand/nextjs/fetch/app/_api/index.ts
+++ @newexpand/nextjs/fetch/app/_api/index.ts
@@ -1,1 +1,11 @@
-import extendFetch from "newexpand-nextjs-fetch";
+import { createCustomFetch } from "newexpand-nextjs-fetch";
+
+const myFetch = createCustomFetch({
+  baseUrl: "https://jsonplaceholder.typicode.com",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export { myFetch };
--- @newexpand/nextjs/fetch/app/page.tsx
+++ @newexpand/nextjs/fetch/app/page.tsx
@@ -1,6 +1,8 @@
-export default function Page() {
-  // const data = extendFetch.get("https://jsonplaceholder.typicode.com/posts/1");
+import { myFetch } from "./_api";
 
-  // console.log(data);
+export default async function Page() {
+  const data = await myFetch.get("/posts/1");
+
+  console.log(data);
   return <h1>Hello, Next.js!</h1>;
 }
